cfdab7be1b44eb0077bc193a99436b5b5cd247f8
Dan Hricko <dhricko9@gmail.com>
2025-12-26 15:12:29 -0500

    changes to project structure

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+version = os.getenv("APP_VERSION", "unknown")
 
 @app.route('/')
 def index():
-    return 'Hello Pittsburgh Steelers!'
+    return 'Hello Pittsburgh Steelers {version}!'
 
 app.run(host='0.0.0.0', port=8080)
